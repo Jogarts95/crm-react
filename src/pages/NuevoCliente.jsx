5c02c0464e3e1694f3cc753c01685c815badd780
Jogarts95 <jogar1995@gmail.com>
2024-01-05 15:24:57 -0600

    Update Layaout component

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,17 @@
 function NuevoCliente() {
   return (
-    <div>NuevoCliente</div>
+    <>
+      <h1 className=" font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className=" mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+
+      <div className=" flex justify-end">
+        <button
+          className=" bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+        >
+          Volver
+        </button>
+      </div>
+    </>
   )
 }
 
